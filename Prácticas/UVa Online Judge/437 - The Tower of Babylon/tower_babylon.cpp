107d5209c3667ed2fd89e0c7e11376ca78af97f4
R.G.A.M. <rgam1989@gmail.com>
2013-04-13 18:46:47 -0400

    UVa 437: Solución parcial.
    Agregado TXT del input.
    Planteando árbol de solución.

--- Prácticas/UVa Online Judge/437 - The Tower of Babylon/tower_babylon.cpp
+++ Prácticas/UVa Online Judge/437 - The Tower of Babylon/tower_babylon.cpp
@@ -1,4 +1,4 @@
-#include "<cstdio>"
+#include <cstdio>
 
 
 template <class C>
@@ -11,39 +11,48 @@ inline void swap (C &a, C &b)
 }
 
 
-template <class C>
-inline void sort (C &x, C &y, C &z)
-{
-	if (x > y)
-		swap(x, y);
-	if (x > z)
-		swap(x, z);
-	if (y > z)
-		swap(y, z);
-}
-
-
 struct Block
 {
 	int x, y, z;
 
-	inline void scanf() {
+	inline void readline() {
 		scanf("%d %d %d", &x, &y, &z);
 	}
 
 	inline void sort() {
-		sort(x, y, z);
+		if (x > y)
+			swap(x, y);
+		if (x > z)
+			swap(x, z);
+		if (y > z)
+			swap(y, z);
 	}
 
 	inline int max_height_alone() {
 		if (z == y || y == x)
 			return z;
+		return x + z;
 	}
 };
 
 
+struct BlockTree
+{
+	int
+		x,
+		y,
+		h;
+	BlockTree
+		*child,
+		*siebling;
+};
+
+
 #define MAX_BLOCKS 30
-Block block[MAX_BLOCKS];
+Block
+	block[MAX_BLOCKS];
+BlockTree
+	block_tree[3*MAX_BLOCKS];
 int partial_height[MAX_BLOCKS];
 
 
@@ -59,12 +68,14 @@ int main()
 
 	while (total_blocks)
 	{
+		maximum_height = 0;
 		for (int i = 0; i < total_blocks; ++i)
 		{
-			current_block = block[i];
-			current_block->scanf();
+			current_block = block + i;
+			current_block->readline();
 			current_block->sort();
-			partial_height[i] = current_block->max_height_alone();
+			maximum_height += partial_height[i] = current_block->max_height_alone();
+			printf("%d: %d (%d, %d, %d)\n", i, partial_height[i], current_block->x, current_block->y, current_block->z);
 		}
 
 		printf("Case %d: maximum height = %d\n", test_case, maximum_height);
