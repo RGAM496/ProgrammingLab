974bc3449f210605ac2b801b455e9a7d37fa1abb
R.G.A.M. <rgam1989@gmail.com>
2013-04-14 03:39:33 -0400

    Tree Summing: Alternativa a la solución.
    Se supone que optimizaba algunos detalles, pero terminó siendo más
    lento.

---
+++ Clases/2013-04-12/Tree Summing/tree_summing_2.cpp
@@ -0,0 +1,122 @@
+#include <cstdio>
+#include <cctype>
+#include <vector>
+
+using namespace std;
+
+
+inline int get_char (int &c)
+{
+	return scanf(" %c", &c);
+}
+
+inline int get_int (int &n)
+{
+	return scanf("%d", &n);
+}
+
+/* Returns false if is an empty tree.
+   Otherwise, returns true and sets it's value to n. */
+inline bool next_tree (int &n)
+{
+	static int c;
+	get_char(c); // c == '('
+	if (get_int(n))
+		return true;
+	get_char(c); // c == ')'
+	return false;
+}
+
+
+std::vector<int> tree_sum(20);
+int depth, compare, vector_capacity;
+
+#define add_depth() \
+	if (++depth == vector_capacity) { \
+		vector_capacity += vector_capacity; \
+		tree_sum.reserve(vector_capacity); \
+	}
+
+
+bool parse_subtree()
+{
+	static int n;
+	bool is_a_leaf;
+
+	is_a_leaf = true;
+
+	if (next_tree(n)) {
+		is_a_leaf = false;
+		n += tree_sum[depth];
+		add_depth();
+		tree_sum[depth] = n;
+		if (parse_subtree())
+			return true;
+		--depth;
+	}
+
+	if (next_tree(n)) {
+		is_a_leaf = false;
+		n += tree_sum[depth];
+		add_depth();
+		tree_sum[depth] = n;
+		if (parse_subtree())
+			return true;
+		--depth;
+	}
+
+	get_char(n);
+
+	if (is_a_leaf) {
+		return compare == tree_sum[depth];
+	}
+
+	return false;
+}
+
+
+inline void go_to_next_test()
+{
+	static int c;
+	while (depth) {
+		get_char(c);
+		switch (c) {
+			case '(':
+				++depth;
+				break;
+			case ')':
+				--depth;
+				break;
+		}
+	}
+}
+
+
+int main ()
+{
+	int n;
+
+	vector_capacity = tree_sum.capacity();
+
+	while (get_int(compare) > 0)
+	{
+		if (next_tree(tree_sum[0]))
+		{
+			depth = 0;
+			if (parse_subtree()){
+				puts("yes");
+				go_to_next_test();
+			}
+			else {
+				puts("no");
+			}
+		}
+		else
+		{
+			puts("no");
+			continue;
+		}
+	}
+
+	return 0;
+}
