9c4da69e83835faf920ba376fefabc88c13527e0
R.G.A.M. <rgam1989@gmail.com>
2013-05-19 00:28:56 -0400

    Ingenuous Cubrency: Boceto de solución.
    Parece funcionar. Se harán ligeras optimizaciones.

--- Clases/2013-05-17/Ingenuous Cubrency/ingenuous_cubrency_1.cpp
+++ Clases/2013-05-17/Ingenuous Cubrency/ingenuous_cubrency_1.cpp
@@ -2,11 +2,13 @@
 
 
 #define MAX_COINS 21
-#define MAX_CUBRENCY 10000
-#define NOT_SET 0
+#define BIG_COINS (MAX_COINS - 1)
+#define LAST_BIG_COIN (BIG_COINS - 1)
+#define MAX_AMMOUNT 10000
 
 typedef unsigned long long int lint;
 typedef unsigned int uint;
+typedef int Change[BIG_COINS];
 
 
 /***************************************************************************/
@@ -28,28 +30,73 @@ struct Coin
 	}
 
 
-	void descomponer (uint n, uint r[MAX_COINS], size_t ini = 0) const {
-		for (; ini < MAX_COINS; ++ini) {
+	inline void descomponer (uint n, Change r, size_t ini = 0) const {
+		for (; ini < BIG_COINS; ++ini) {
 			r[ini] = n / c[ini];
 			n = n % c[ini];
 		}
+	}
+
+	inline size_t biggest_index (Change r, size_t i = 0) {
+		for (; i < LAST_BIG_COIN; ++i)
+			if (r[i])
+				break;
+		return i;
 	}
 };
 
 
 /***************************************************************************/
 
-
 Coin coin;
-lint cubrencies[MAX_CUBRENCY] = {NOT_SET};
+lint cubrencies[MAX_AMMOUNT][BIG_COINS];
+
+
+lint count_cubrencies (size_t n, Change change, size_t ini)
+{
+	int &count = change[ini];
+	uint &value = coin[ini];
+	size_t n_aux, ini_aux;
+	lint &ans = cubrencies[n][ini], partial_ans;
+
+	if (ini == LAST_BIG_COIN)
+		return count + 1;
+
+	++ini;
+	n_aux = n - count * value;
+
+	for (; count >= 0; --count, n_aux += value)
+	{
+		coin.descomponer (n_aux, change, ini);
+		ini_aux = ini; //coin.biggest_index (change, ini);
+		partial_ans = cubrencies[n_aux][ini_aux]
+			? cubrencies[n_aux][ini_aux]
+			: count_cubrencies (n_aux, change, ini_aux);
+		for (size_t i = ini; i < ini_aux; ++i)
+			cubrencies[n][i] += partial_ans;
+		ans += partial_ans;
+	}
+
+	return ans;
+}
 
 
 lint possible_cubrencies (size_t n)
 {
-	int cubrency[MAX_COINS];
+	Change change;
+	size_t degree;
+	lint ans;
 
-	if (cubrencies[n])
-		return cubrencies[n];
+	if (cubrencies[n][0])
+		return cubrencies[n][0];
+
+	coin.descomponer (n, change);
+	degree = coin.biggest_index (change);
+	ans = count_cubrencies (n, change, degree);
+	for (size_t i = 0; i < degree; ++i)
+		cubrencies[n][i] = ans;
+
+	return ans;
 };
 
 
@@ -58,5 +105,8 @@ lint possible_cubrencies (size_t n)
 
 int main ()
 {
+	uint ammount;
+	while (scanf ("%d", &ammount) == 1)
+		printf("%llu\n", possible_cubrencies(ammount));
 	return 0;
 }
