3d9008c058824c9158faecd77c47204c7860e0b8
R.G.A.M. <rgam1989@gmail.com>
2013-05-26 17:22:57 -0400

    Weights and Measures: Continuando con el segundo planteamiento.

--- Clases/2013-05-24/Weights and Measures/weights_measures_2.cpp
+++ Clases/2013-05-24/Weights and Measures/weights_measures_2.cpp
@@ -6,6 +6,24 @@
 /*********************************************************************/
 
 
+template <typename N>
+inline void min (N &d, const N &s)
+{
+	if (d > s)
+		d = s;
+}
+
+template <typename N>
+inline void max (N &d, const N &s)
+{
+	if (d < s)
+		d = s;
+}
+
+
+/*********************************************************************/
+
+
 template <class v_type, size_t MAX_EDGES>
 struct Node
 {
@@ -35,6 +53,7 @@ struct Graph
 	GraphNode first[SIZE], *last;
 
 	inline void clear () {last = first;}
+	inline size_t size () {return last - first;}
 
 	inline void push (const v_type &v) {last->clear(); last->value = v; ++last;}
 	inline void push () {last->clear(); ++last;}
@@ -81,6 +100,7 @@ struct Turtle
 /*********************************************************************/
 
 typedef Graph <Turtle *, MAX_TURTLES> GraphTurtles;
+typedef GraphTurtles::GraphNode TurtleNode;
 
 Turtle turtle[MAX_TURTLES], turtle_aux[MAX_TURTLES];
 GraphTurtles graph_turtles;
@@ -94,25 +114,52 @@ inline void init ()
 }
 
 
+int max_stack (TurtleNode *vertex, size_t not_marked)
+{
+	return 0;
+}
+
+
+inline int max_stack ()
+{
+	TurtleNode *vertex;
+	int result, r_aux;
+	size_t not_marked;
+
+	not_marked = graph_turtles.size() - 1;
+	result = 0;
+	for (vertex = graph_turtles.first; vertex < graph_turtles.last; ++vertex)
+	{
+		vertex->value->marked = true;
+		r_aux = max_stack(vertex, not_marked);
+		min(result,r_aux);
+		vertex->value->marked = false;
+	}
+
+	return 0;
+}
+
+
 /*********************************************************************/
 
 
 int main ()
 {
 	Turtle *t = turtle;
-	GraphTurtles::GraphNode
+	TurtleNode
 		*n_aux[2],
 		*&node = n_aux[0],
 		*&last_node = n_aux[1] = graph_turtles.first;
 
 	init();
 
+	graph_turtles.clear();
 	while (scanf("%d %d", &t->weight, &t->strength) == 2)
 	{
+		graph_turtles.push();
 		t->strength -= t->weight;
 		t->marked = false;
 		++t;
-		last_node->clear();
 		for (node = graph_turtles.first; node < last_node; ++node)
 		{;
 			Turtle t_aux[2];
