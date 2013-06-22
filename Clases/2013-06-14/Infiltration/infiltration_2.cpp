8a2c8d52a7bbcaf0accb4c57d359537f9ff85113
R.G.A.M. <rgam1989@gmail.com>
2013-06-21 23:49:45 -0400

    Infiltration: Segundo planteamiento funcional.
    Supera el tiempo de ejecución.

--- Clases/2013-06-14/Infiltration/infiltration_2.cpp
+++ Clases/2013-06-14/Infiltration/infiltration_2.cpp
@@ -30,6 +30,48 @@ inline int get_bit (int &n)
 }
 
 
+template <typename N>
+inline void set_min (N &a, const N &b)
+{
+	if (a > b)
+		a = b;
+}
+
+
+template <typename N>
+inline N min (const N &a, const N &b)
+{
+	return a < b ? a : b;
+}
+
+
+/*********************************************************/
+
+
+template <typename C, size_t SIZE = MAX_CELLS>
+struct Stack
+{
+	C first[SIZE], *last;
+
+	inline void clear () {last = first;}
+	inline void push (const C &c) {*last = c; ++last;}
+	inline void push () {++last;}
+	inline void pop () {--last;}
+	inline size_t size () const {return last - first;}
+	inline C & operator [] (size_t n) {return first[n];}
+	inline C & front () {return *first;}
+	inline C & back () {return last[-1];}
+
+	inline Stack <C, SIZE> & operator = (const Stack <C, SIZE> &st) {
+		clear ();
+		for (const C *p = st.first; p < st.last; ++p) {
+			push (*p);
+		}
+		return *this;
+	}
+};
+
+
 /*********************************************************/
 
 
@@ -83,11 +125,10 @@ struct Cell
 	inline int & operator [] (const size_t n) {return control[n];}
 	inline int operator [] (const size_t n) const {return control[n];}
 
-	inline Cell & operator = (const Cell &c)
+	inline void operator = (const Cell &c)
 	{
 		number = c.number;
 		control.controlled_cells = c.control.controlled_cells;
-		return *this;
 	}
 };
 
@@ -112,11 +153,17 @@ Cell cell_aux[MAX_CELLS], cell[MAX_CELLS];
 
 inline void init ()
 {
-	size_t i;
+	size_t i, j, l, pot2;
 
-	for (i = 1; i <= MAX_CELLS; ++i)
+	pot2 = 1;
+	l = 2;
+	for (i = 1; i <= MAX_CELLS; ++l)
 	{
-		
+		pot2 <<= 1;
+		for (j = 0; j < pot2 && i <= MAX_CELLS; ++j, ++i)
+		{
+			limit[i] = l;
+		}
 	}
 
 	for (i = 0; i < MAX_CELLS; ++i)
@@ -164,14 +211,86 @@ inline void sort_cells ()
 /*********************************************************/
 
 
+Stack <int> solution, solution_aux;
+Stack <Control> control;
+
+
+int solve (int start, int current_size, int &l)
+{
+	int i;
+
+	#ifdef DEBUG_SOLVE
+	fprintf(stderr, "\t(%d) %d: ", l, current_size);
+	for (i = 0; i < CELLS; ++i)
+		fprintf(stderr, "%d", control[current_size-1][i]);
+	for (i = 0; i < current_size; ++i)
+		fprintf(stderr, " %d", solution_aux[i]);
+	fputc ('\n', stdout);
+	#endif
+
+	if (current_size < l)
+	{
+		Control &c = control[current_size - 1];
+		if (c.controlled_cells == CELLS)
+		{
+			l = current_size;
+			solution = solution_aux;
+		}
+		else
+		{
+			control.push ();
+			Control &c_aux = control[current_size];
+			++current_size;
+			for (int i = start + 1; i < CELLS; ++i)
+			{
+				solution_aux.push (i);
+				c_aux.combine (cell[i].control, c);
+				solve (i, current_size, l);
+				solution_aux.pop ();
+			}
+			control.pop ();
+		}
+	}
+
+	return l;
+}
+
+
+int solve ()
+{
+	int l, i, r;
+
+	r = l = limit[CELLS];
+	solution_aux. clear ();
+	control.clear ();
+
+	control.push ();
+	for (i = 0; i < CELLS; ++i)
+	{
+		solution_aux.push (i);
+		control[0] = cell[i].control;
+		set_min (r, solve (i, 1, l));
+		solution_aux.pop ();
+	}
+	control.pop ();
+
+	return l;
+}
+
+
+/*********************************************************/
+
+
 int main ()
 {
-	int i, j;
+	int i, j, r, test_case;
 
 	init ();
+	test_case = 0;
 
 	while (get_int (CELLS) == 1)
 	{
+		++test_case;
 		for (i = 0; i < CELLS; ++i)
 		{
 			Cell &c = cell_aux[i];
@@ -183,6 +302,11 @@ int main ()
 		}
 
 		sort_cells ();
+		r = solve ();
+		printf("Case %d: %d", test_case, r);
+		for (size_t i = 0; i < r; ++i)
+			printf(" %d", cell[solution[i]].number + 1);
+		putchar ('\n');
 	}
 
 	return 0;
